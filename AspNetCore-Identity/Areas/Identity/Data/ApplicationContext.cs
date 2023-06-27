b4a909016b9a1d531b7f09fe85fd623b2ff152c5
Hihiz <public@mail.ru>
2023-06-27 21:00:26 +0300

    Сделать класс контекста и пользователя

---
+++ AspNetCore-Identity/Areas/Identity/Data/ApplicationContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCore_Identity.Areas.Identity.Data;
+
+public class ApplicationContext : IdentityDbContext<ApplicationIdentityUser>
+{
+    public ApplicationContext(DbContextOptions<ApplicationContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
+}
---
+++ AspNetCore-Identity/Areas/Identity/Data/ApplicationIdentityUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AspNetCore_Identity.Areas.Identity.Data;
+
+public class ApplicationIdentityUser : IdentityUser
+{
+    [PersonalData]
+    [Column(TypeName = "nvarchar(100)")]
+    public string FirstName { get; set; }
+
+    [PersonalData]
+    [Column(TypeName = "nvarchar(100)")]
+    public string LastName { get; set; }
+}
+
